5eee82b00dc75ebf33b61ebd1424245ed74ed669
Jamie Wilson <howemanning@gmail.com>
2023-12-15 00:56:48 +1300

    Fetch AdviceSlip API with JS

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("dice-button").addEventListener("click", function () {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      const advice = data.slip.advice;
+      const adviceId = data.slip.id;
+      document.getElementById("advice-text").textContent = `"${advice}"`;
+      document.getElementById("advice-id").textContent = `Advice #${adviceId}`;
+    })
+    .catch((error) => {
+      console.error("Error fetching advice:", error);
+      // Handle the error (e.g., show an error message)
+    });
+});
